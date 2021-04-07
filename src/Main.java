d9ceb2ac84965ab3de6907f57714b99f715d6314
jvela <jose.vela.jr@outlook.com>
2021-04-07 11:15:33 -0300

    Exercicio115.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import entities.Order;
+import enumcompos.OrderStatus;
+
 import java.text.ParseException;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
+
+
 
     }
 }
---
+++ src/enumcompos/Exercicio115.java
@@ -0,0 +1,26 @@
+package enumcompos;
+
+import entities.Order;
+
+import java.util.Date;
+
+public class Exercicio115 {
+
+    public void exercicio115(){
+
+        Order order = new Order(10,new Date(), OrderStatus.DELIVERED);
+
+        System.out.println(order.getId());
+        System.out.println(order.getMoment());
+        System.out.println(order.getStatus());
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
+
+    }
+}
